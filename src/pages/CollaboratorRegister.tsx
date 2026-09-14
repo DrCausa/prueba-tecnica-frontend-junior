9c96e70ac1a450d59cdb87c2ca9f6a1660e6171f
DrCausa <leo10said@gmail.com>
2026-09-14 16:39:49 -0500

    feat(collaborators): add zod validation schema and types for registration

--- src/pages/CollaboratorRegister.tsx
+++ src/pages/CollaboratorRegister.tsx
@@ -1,3 +1,21 @@
+import * as z from "zod"
+
+const roles = ["Admin", "Scrum Master", "Tech Lead", "Developer"] as const
+
+const registerSchema = z.object({
+  first_name: z.string().min(1, "El nombre es requerido"),
+  last_name: z.string().min(1, "El apellido es requerido"),
+  email: z.email("Formato de correo inválido").min(1, "El correo es requerido"),
+  phone: z.string().min(1, "El teléfono es requerido"),
+  role: z.enum(roles, { error: "Debes proporcionar un rol válido" }),
+  password: z.string()
+  .min(8, "Debe tener al menos 8 caracteres")
+  .regex(/[A-Z]/, "Debe contener al menos una letra mayúscula")
+  .regex(/[0-9]/, "Debe contener al menos un número")
+})
+
+type RegisterFormValues = z.infer<typeof registerSchema>
+
 const CollaboratorRegister = () => {
   return (
     <div>CollaboratorRegister</div>
